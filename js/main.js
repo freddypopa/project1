07359ea5fad82f4f072dd1782b4e6a336e735b2c
freddypopa <gamersuper801@gmail.com>
2023-05-13 22:42:47 +0300

    changed slider

--- js/main.js
+++ js/main.js
@@ -1,16 +1,28 @@
 $(function () {
 
   $('.history__items').slick({
-    dots: false,
-    slidesToShow: 1,
-    slidesToScroll: 1,
+    arrows: false,
+    slidesToShow: 3,
+    responsive: [{
+      breakpoint: 900,
+      settings:{
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+      }
+    }]
   });
 
-  $('.menu__btn , .menu__list-link , .info__list-title ').on('click', function (e) {
-    e.peventDefault;
+  $('.menu__btn , .menu__list-link').on('click', function (e) {
+    e.preventDefault;
     $('.menu__btn').toggleClass('menu__btn--active');
-    $('.info__list-item').toggleClass('info__list-item--active');
     $('.header-top__btn').toggleClass('header-top__btn--active');
     $('.menu__list').toggleClass('menu__btn--active');
+    $('body').toggleClass('test')
+  })
+  $('.info__list-title').on('click', function (e) {
+    e.preventDefault;
+    $('.info__list-item').toggleClass('info__list-item--active');
+    
   })
 });
