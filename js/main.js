8251f8f80b1fb9cb1e91e4ed678c53d3384d912d
freddypopa <gamersuper801@gmail.com>
2023-05-14 18:02:52 +0300

    ended adaptive

--- js/main.js
+++ js/main.js
@@ -3,14 +3,24 @@ $(function () {
   $('.history__items').slick({
     arrows: false,
     slidesToShow: 3,
-    responsive: [{
-      breakpoint: 900,
+    responsive: [
+      {
+      breakpoint: 1050,
+      settings:{
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+      }
+    },
+      {
+      breakpoint: 747,
       settings:{
           slidesToShow: 1,
           slidesToScroll: 1,
           infinite: true,
       }
-    }]
+    }
+  ]
   });
 
   $('.menu__btn , .menu__list-link').on('click', function (e) {
@@ -20,9 +30,28 @@ $(function () {
     $('.menu__list').toggleClass('menu__btn--active');
     $('body').toggleClass('test')
   })
-  $('.info__list-title').on('click', function (e) {
+
+
+  $('.info__list-title-p').on('click', function (e) {
     e.preventDefault;
-    $('.info__list-item').toggleClass('info__list-item--active');
-    
+    $('.info__list-p').toggleClass('info__list-p--active');
   })
+  $('.info__list-title-r').on('click', function (e) {
+    e.preventDefault;
+    $('.info__list-r').toggleClass('info__list-r--active');
+  })
+  $('.info__list-title-re').on('click', function (e) {
+    e.preventDefault;
+    $('.info__list-re').toggleClass('info__list-re--active');
+  })
+  $('.info__list-title-c').on('click', function (e) {
+    e.preventDefault;
+    $('.info__list-c').toggleClass('info__list-c--active');
+  })
+  $('.info__list-title-t').on('click', function (e) {
+    e.preventDefault;
+    $('.info__list-t').toggleClass('info__list-t--active');
+  })
+
+
 });
